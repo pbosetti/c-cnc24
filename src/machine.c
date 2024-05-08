da90894b20498550c606f0aea7021de02c0c7f8d
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-05-08 14:05:16 +0200

    After lesson 20240508

--- src/machine.c
+++ src/machine.c
@@ -10,6 +10,8 @@
 #include "machine.h"
 #include "toml.h"
 #include <mqtt_protocol.h>
+#include <signal.h> // for signal handling
+#include <unistd.h> // for usleep
 
 /*
   ____        __ _       _ _   _
@@ -28,15 +30,22 @@ typedef struct machine {
   data_t fmax;                  // Maximum feedrate (mm/min)
   point_t *zero;                // Initial machine position
   point_t *setpoint, *position; // Setpoint and actual position
+  point_t *offset;              // Workpiece origin coordinates
   /* MQTT SECTION */
   char broker_address[BUFLEN];
   int broker_port;
-  char pub_topc[BUFLEN];
-  char sub_topc[BUFLEN];
+  char pub_topic[BUFLEN];
+  char sub_topic[BUFLEN];
   char msg_buffer[BUFLEN];
   struct mosquitto *mqt;
   struct mosquitto_message *msg;
+  int connecting;
 } machine_t;
+
+// MQTT Callbacks:
+static void on_connect(struct mosquitto *, void *, int);
+static void on_message(struct mosquitto *, void *,
+                       const struct mosquitto_message *);
 
 /*
   _____                 _   _
@@ -70,9 +79,12 @@ machine_t *machine_new(char const *config_path) {
   m->zero = point_new();
   m->setpoint = point_new();
   m->position = point_new();
+  m->offset = point_new();
+  m->connecting = 1;
   point_set_xyz(m->zero, 0, 0, 0);
   point_set_xyz(m->setpoint, 0, 0, 0);
   point_set_xyz(m->position, 0, 0, 0);
+  point_set_xyz(m->offset, 0, 0, 0);
 
   // 2. Open the INI file ======================================================
   ini_file = fopen(config_path, "r");
@@ -144,6 +156,13 @@ machine_t *machine_new(char const *config_path) {
     point_set_xyz(m->zero, toml_double_at(point, 0).u.d,
                   toml_double_at(point, 1).u.d, toml_double_at(point, 2).u.d);
   }
+  point = toml_array_in(ccnc, "offset");
+  if (!point) {
+    wprintf("Missing C-CNC:offset, using default\n");
+  } else {
+    point_set_xyz(m->offset, toml_double_at(point, 0).u.d,
+                  toml_double_at(point, 1).u.d, toml_double_at(point, 2).u.d);
+  }
 
   // read the MQTT section
   toml_table_t *mqtt = toml_table_in(conf, "MQTT");
@@ -197,6 +216,7 @@ machine_getter(data_t, fmax);
 machine_getter(point_t *, zero);
 machine_getter(point_t *, setpoint);
 machine_getter(point_t *, position);
+machine_getter(int, connecting);
 
 /* METHODS ********************************************************************/
 
@@ -210,8 +230,8 @@ void machine_print_params(machine_t const *m, FILE *out) {
           point_x(m->zero), point_y(m->zero), point_z(m->zero));
   fprintf(out, BBLK "MQTT:broker_addr: " CRESET "%s\n", m->broker_address);
   fprintf(out, BBLK "MQTT:broker_port: " CRESET "%d\n", m->broker_port);
-  fprintf(out, BBLK "MQTT:pub_topic: " CRESET "%s\n", m->pub_topc);
-  fprintf(out, BBLK "MQTT:sub_topic: " CRESET "%s\n", m->sub_topc);
+  fprintf(out, BBLK "MQTT:pub_topic: " CRESET "%s\n", m->pub_topic);
+  fprintf(out, BBLK "MQTT:sub_topic: " CRESET "%s\n", m->sub_topic);
 }
 
 // Connect with the broker and setup callbacks
@@ -234,16 +254,100 @@ ccnc_error_t machine_connect(machine_t *m, machine_on_message callback) {
 
 // synchronize the machine object setpoint with the physical machine
 // i.e. publish m->setpoint via MQTT
-ccnc_error_t machine_sync(machine_t *m, int rapid) {}
+ccnc_error_t machine_sync(machine_t *m, int rapid) {
+  assert(m && m->mqt);
+  // Transmit a setpoint description in JSON like this:
+  // {"x":1.1,"y":23.0,"z":123.0,"rapid":1}
+  snprintf(m->msg_buffer, BUFLEN, "{\"x\":%f,\"y\":%f,\"z\":%f,\"rapid\":%d}",
+           point_x(m->setpoint) + point_x(m->offset),
+           point_y(m->setpoint) + point_y(m->offset),
+           point_z(m->setpoint) + point_z(m->offset), rapid);
+  if (mosquitto_publish(m->mqt, NULL, m->pub_topic, strlen(m->msg_buffer),
+                        m->msg_buffer, 0, 0) != MOSQ_ERR_SUCCESS) {
+    eprintf("Could not send message %s\n", m->msg_buffer);
+    return MQTT_ERR;
+  }
+  mosquitto_loop(m->mqt, 1, 1);
+  return NO_ERR;
+}
 
 // enable receiving MQTT messages from physical machine
-ccnc_error_t machine_listen_start(machine_t *m) {}
+ccnc_error_t machine_listen_start(machine_t *m) {
+  assert(m && m->mqt);
+  if (mosquitto_subscribe(m->mqt, NULL, m->sub_topic, 0) != MOSQ_ERR_SUCCESS) {
+    eprintf("Could not subscribe to topic %s\n", m->sub_topic);
+    return MQTT_ERR;
+  }
+  wprintf("Subscribed to topic %s\n", m->sub_topic);
+  return NO_ERR;
+}
 
 // disable receiving MQTT messages from physical machine
-ccnc_error_t machine_listen_stop(machine_t *m) {}
+ccnc_error_t machine_listen_stop(machine_t *m) {
+  assert(m && m->mqt);
+  if (mosquitto_unsubscribe(m->mqt, NULL, m->sub_topic) != MOSQ_ERR_SUCCESS) {
+    eprintf("Could not unsubscribe from %s\n", m->sub_topic);
+    return MQTT_ERR;
+  }
+  wprintf("Unsubscribed from topic %s\n", m->sub_topic);
+  return NO_ERR;
+}
 
 // Disconnect from MQTT broker and stop network operations
-void machine_disconnect(machine_t *m) {}
+void machine_disconnect(machine_t *m) {
+  assert(m && m->mqt);
+  // Wait for network ops to be completed
+  while (mosquitto_want_write(m->mqt)) {
+    mosquitto_loop(m->mqt, 1, 1);
+    usleep(10000); // avoid consuming 100% of a CPU core for nothing
+  }
+  mosquitto_disconnect(m->mqt);
+  m->connecting = 1;
+}
+
+/*
+  ____  _        _   _         __                  _   _
+ / ___|| |_ __ _| |_(_) ___   / _|_   _ _ __   ___| |_(_) ___  _ __  ___
+ \___ \| __/ _` | __| |/ __| | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+  ___) | || (_| | |_| | (__  |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+ |____/ \__\__,_|\__|_|\___| |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+
+*/
+static void on_connect(struct mosquitto *mqtt, void *obj, int rc) {
+  machine_t *m = (machine_t *)obj;
+  if (rc == CONNACK_ACCEPTED) {
+    wprintf("Connection successful to %s:%d\n", m->broker_address,
+            m->broker_port);
+    if (mosquitto_subscribe(mqtt, NULL, m->sub_topic, 0) != MOSQ_ERR_SUCCESS) {
+      eprintf("Could not subscribe to %s\n", m->sub_topic);
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    eprintf("Connection error");
+    exit(EXIT_FAILURE);
+  }
+  m->connecting = 0;
+}
+
+// Deal with messages coming on topic m->sub_topic, e.g. "c-cnc/status/#"
+// Typ. # can be either "error" or "position"
+static void on_message(struct mosquitto *mqtt, void *obj,
+                       const struct mosquitto_message *msg) {
+  machine_t *m = (machine_t *)obj;
+  mosquitto_message_copy(m->msg, msg);
+  // get last component of topic:
+  char *subtopic = strrchr(msg->topic, '/') + 1;
+  if (strcmp(subtopic, "error") == 0) {
+    m->error = atof(msg->payload);
+  } else if (strcmp(subtopic, "position") == 0) {
+    char *nxt = msg->payload;
+    point_set_x(m->position, strtod(nxt, &nxt));
+    point_set_y(m->position, strtod(nxt + 1, &nxt));
+    point_set_z(m->position, strtod(nxt + 1, &nxt));
+  } else {
+    eprintf("Got unexpected subtopic %s\n", msg->topic);
+  }
+}
 
 /*
   __  __            _     _              _            _
@@ -254,9 +358,19 @@ void machine_disconnect(machine_t *m) {}
 
 */
 #ifdef MACHINE_MAIN
+static int _running = 1;
+
+void sighandler(int signum) { _running = 0; }
+
 int main(int argc, char const **argv) {
   // variables declatarion
   machine_t *m = NULL;
+  point_t *pos = NULL;
+  char *p_desc = NULL;
+
+  // install a signal handler
+  signal(SIGINT, sighandler);
+
   // Check command line arguments
   if (argc != 2) {
     eprintf("Please provide the path to a, INI file! \n");
@@ -270,8 +384,28 @@ int main(int argc, char const **argv) {
   }
   // Print machine parameters
   machine_print_params(m, stdout);
+
+  // Test MQTT connectivity
+  machine_connect(m, &on_message);
+  machine_listen_start(m);
+  while (_running) {
+    machine_sync(m, 0);
+    pos = machine_position(m);
+    point_inspect(pos, &p_desc);
+    // use \r rather than \n to go back with cursor at the beginning 
+    // of the same line rather than on a new line
+    // Note: \r does not update the console, as \n does, so we need to 
+    // manually call fflush(stout) eventually
+    printf("Position: %s, error: %.3f\r", p_desc, machine_error(m));
+    usleep(10000);
+    fflush(stdout);
+  }
+
   // free memory
+  machine_listen_stop(m);
+  machine_disconnect(m);
   machine_free(m);
+  free(p_desc);
   return 0;
 }
 
--- src/machine.h
+++ src/machine.h
@@ -1,10 +1,10 @@
 /*
-  __  __            _     _            
- |  \/  | __ _  ___| |__ (_)_ __   ___ 
+  __  __            _     _
+ |  \/  | __ _  ___| |__ (_)_ __   ___
  | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \
  | |  | | (_| | (__| | | | | | | |  __/
  |_|  |_|\__,_|\___|_| |_|_|_| |_|\___|
-                                       
+
 */
 #ifndef MACHINE_H
 #define MACHINE_H
@@ -14,30 +14,28 @@
 #include <mosquitto.h>
 
 /*
-  ____            _                 _   _                 
- |  _ \  ___  ___| | __ _ _ __ __ _| |_(_) ___  _ __  ___ 
+  ____            _                 _   _
+ |  _ \  ___  ___| | __ _ _ __ __ _| |_(_) ___  _ __  ___
  | | | |/ _ \/ __| |/ _` | '__/ _` | __| |/ _ \| '_ \/ __|
  | |_| |  __/ (__| | (_| | | | (_| | |_| | (_) | | | \__ \
  |____/ \___|\___|_|\__,_|_|  \__,_|\__|_|\___/|_| |_|___/
-                                                          
+
 */
 
 typedef struct machine machine_t;
 
-
 /*
-  _____                 _   _                 
- |  ___|   _ _ __   ___| |_(_) ___  _ __  ___ 
+  _____                 _   _
+ |  ___|   _ _ __   ___| |_(_) ___  _ __  ___
  | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
  |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
-                                              
+
 */
 
 /* LIFECYCLE ******************************************************************/
 machine_t *machine_new(char const *config_path);
 void machine_free(machine_t *m);
-
 
 /* ACCESSORS ******************************************************************/
 data_t machine_A(machine_t const *m);
@@ -48,7 +46,7 @@ data_t machine_fmax(machine_t const *m);
 point_t *machine_zero(machine_t const *m);
 point_t *machine_setpoint(machine_t const *m);
 point_t *machine_position(machine_t const *m);
-
+int machine_connecting(machine_t const *m);
 
 /* METHODS ********************************************************************/
 void machine_print_params(machine_t const *m, FILE *out);
@@ -56,14 +54,13 @@ void machine_print_params(machine_t const *m, FILE *out);
 /* MQTT related */
 
 // typedef for on_message callback signature
-typedef void (* machine_on_message)(struct mosquitto *, void *, const struct mosquitto_message *)
+typedef void (*machine_on_message)(struct mosquitto *, void *,
+                                   const struct mosquitto_message *);
 
 ccnc_error_t machine_connect(machine_t *m, machine_on_message callback);
 ccnc_error_t machine_sync(machine_t *m, int rapid);
 ccnc_error_t machine_listen_start(machine_t *m);
 ccnc_error_t machine_listen_stop(machine_t *m);
 void machine_disconnect(machine_t *m);
-
-
 
 #endif // MACHINE_H
